c0dfa5a94758c7c9e90f79267e6dd12d08d18109
Egor <korchagin.egorka1@gmail.com>
2021-04-12 09:50:00 +1000

    Initial Commit

---
+++ SwitchMode/SwitchModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchModeApp.swift
+//  SwitchMode
+//
+//  Created by Egor Korchagin on 12.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwitchModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
